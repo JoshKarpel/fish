6f5ff7d82ef5cac496000fb637cddfa617dc7189
JoshKarpel <karpel@wisc.edu>
2019-07-10 08:11:18 -0500

    refactoring

--- playground.py
+++ playground.py
@@ -8,7 +8,9 @@ from tqdm import tqdm
 # opencv color order is BGR
 
 def read(path):
-    cap = cv.VideoCapture(path)
+    path = Path(path)
+
+    cap = cv.VideoCapture(str(path))
 
     num_frames = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
     height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
@@ -18,7 +20,7 @@ def read(path):
     # BUT R == G == B, because its grayscale
     # so we can lose the RGB dimension
     frames = np.empty((num_frames, height, width), dtype = np.uint8)
-    for current_frame in tqdm(range(num_frames)):
+    for current_frame in tqdm(range(num_frames), desc = f'Reading frames from {path}'):
         _, frame = cap.read()
         frames[current_frame] = frame[:, :, 0]  # grab first channel
 
@@ -35,6 +37,17 @@ def save(path, frames):
 def load(path):
     with Path(path).open(mode = 'rb') as file:
         return np.load(file)
+
+
+def load_or_read(path):
+    path = Path(path)
+
+    try:
+        frames = load(path.with_suffix('.npy'))
+    except FileNotFoundError:
+        frames = read(path.with_suffix('.avi'))
+        save(path.with_suffix('.npy'), frames)
+    return frames
 
 
 def display(frames, wait = 0):
@@ -60,10 +73,12 @@ def display(frames, wait = 0):
             break
 
 
-def make_movie(path, frames):
-    writer = None
+def make_movie(path, frames, num_frames = None):
+    path = Path(path)
+    path.parent.mkdir(parents = True, exist_ok = True)
 
-    for frame in tqdm(frames, desc = 'Writing frames for movie'):
+    writer = None
+    for frame in tqdm(frames, total = num_frames, desc = 'Writing frames for movie'):
         if writer is None:
             height, width = frame.shape
             fourcc = cv.VideoWriter_fourcc(*'mp4v')  # Be sure to use lower case
@@ -84,11 +99,11 @@ def streaming_std(frames):
     return np.sqrt(std_squared / len(frames)).astype(np.uint8)
 
 
-def process_frames(frames):
+def process_frames(frames, threshold = 0):
     avg_over_frames = np.mean(frames, axis = 0)
     std_over_frames = streaming_std(frames)
 
-    avg_plus_std = avg_over_frames + (2 * std_over_frames)
+    threshold = avg_over_frames + (threshold * std_over_frames)
     frame_minus_background = None
     for frame in frames:
         if frame_minus_background is None:
@@ -96,7 +111,7 @@ def process_frames(frames):
 
         frame_minus_background[:] = frame - avg_over_frames
         frame_minus_background[:] = np.where(
-            frame >= avg_plus_std,
+            frame >= threshold,
             frame_minus_background,
             0
         )
@@ -105,10 +120,14 @@ def process_frames(frames):
 
 
 if __name__ == '__main__':
-    # frames = read('data/control.avi')
-    # save('data/control.npy', frames)
+    IN = Path.cwd() / 'data'
+    OUT = Path.cwd() / 'out'
 
-    frames = load('data/control.npy')
-
-    # display(frames_for_display(frames), wait = 10)
-    make_movie('test.mp4', process_frames(frames))
+    for data in ['control', 'drug']:
+        for threshold in [0, .1, .2, .5, 1, 2, 3]:
+            frames = load_or_read(IN / data)
+            make_movie(
+                OUT / f'test_{data}_threshold={threshold:.2f}.mp4',
+                process_frames(frames),
+                num_frames = len(frames),
+            )
