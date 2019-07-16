48db7f85366d5438272bc5dda78743b07d6b24de
JoshKarpel <karpel@wisc.edu>
2019-07-15 20:09:10 -0500

    update pca-map

--- dev/pca-map.py
+++ dev/pca-map.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import itertools
+import sys
 
 import fish
 import htmap
@@ -7,34 +8,42 @@ import htmap
 
 @htmap.mapped(map_options = htmap.MapOptions(
     request_disk = '10GB',
-    request_memory = '4GB',
+    request_memory = '8GB',
 ))
-def label_movie(movie, dimensions, clusters):
-    op = f'{movie}__dims={dimensions}_clusters={clusters}.mp4'
+def label_movie(movie, dimensions, clusters, remove_background = False):
+    op = f'{movie}__dims={dimensions}_clusters={clusters}_rmv={remove_background}.mp4'
 
     fish.label_movie(
         input_movie = f'{movie}.avi',
         output_path = op,
         pca_dimensions = dimensions,
         clusters = clusters,
+        remove_background = remove_background,
     )
 
-    htmap.transfer_output(op)
+    htmap.transfer_output_files(op)
 
 
 if __name__ == '__main__':
+    tag_prefix = sys.argv[1]
+    docker_version = sys.argv[2]
+
+    htmap.settings['DOCKER.IMAGE'] = f'maventree/fish:{docker_version}'
+
     movies = ['control', 'drug']
     dimensions = [2, 5, 10]
     clusters = [2, 4, 8]
+    remove_bgnds = [False, True]
 
     for movie in movies:
-        with label_movie.map_builder(
+        with label_movie.build_map(
+            tag = f'{tag_prefix}-{movie}',
             map_options = htmap.MapOptions(
-                shared_input_files = [f'http://proxy.chtc.wisc.edu/SQUID/karpel/{movie}.avi']
+                fixed_input_files = [f'http://proxy.chtc.wisc.edu/SQUID/karpel/{movie}.avi']
             )
         ) as mb:
-            for dim, clu in itertools.product(dimensions, clusters):
-                mb(movie, dim, clu)
+            for dim, clu, rmv in itertools.product(dimensions, clusters, remove_bgnds):
+                mb(movie, dim, clu, remove_background = rmv)
 
         map = mb.map
         print(f'Submitted {map} with {len(map)} jobs')
