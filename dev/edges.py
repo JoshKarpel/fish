37432880b110d0f3648591491759e4c9b4204558
JoshKarpel <karpel@wisc.edu>
2019-10-07 17:04:06 -0500

    cleanup

--- dev/edges.py
+++ dev/edges.py
@@ -14,7 +14,7 @@ def make_frames(frames, lower, upper, smoothing, draw_on_original=True):
     open_kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (4, 4))
     close_kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (7, 7))
     backsub = cv.createBackgroundSubtractorKNN()
-    oj = fish.ObjectTracker()
+    tracker = fish.ObjectTracker()
 
     for frame_idx, frame in enumerate(frames):
         mod = backsub.apply(frame)
@@ -22,13 +22,14 @@ def make_frames(frames, lower, upper, smoothing, draw_on_original=True):
         mod = cv.morphologyEx(mod, cv.MORPH_CLOSE, close_kernel)
         edges = fish.get_edges(mod, lower, upper, smoothing)
         contours = fish.get_contours(edges)
+
         if frame_idx > 10:
-            fish.track_objects(oj, contours, frame_idx)
-            oj.clean(frame_idx)
+            tracker.track(contours, frame_idx)
+            tracker.clean(frame_idx)
 
         img = cv.cvtColor((frame if draw_on_original else mod), cv.COLOR_GRAY2BGR)
-        img = fish.draw_contours(img, contours)
-        img = fish.draw_objects(img, oj)
+        img = fish.draw_bounding_rectangles(img, contours)
+        img = fish.draw_live_object_tracks(img, tracker)
         yield img
 
 
@@ -40,12 +41,9 @@ if __name__ == "__main__":
     for movie in ["drug", "control"]:
         frames = fish.load_or_read(IN / movie)[100:]
 
-        # frames = fish.remove_background(frames, threshold=0)
-
-        for d in [True, False]:
-            op = fish.make_movie(
-                OUT / f"edge_test__{movie}__draw_on_original={d}",
-                frames=make_frames(frames, 100, 200, 5, draw_on_original=d),
-                num_frames=len(frames),
-                fps=5,
-            )
+        op = fish.make_movie(
+            OUT / f"edge_test__{movie}",
+            frames=make_frames(frames, 100, 200, 5, draw_on_original=True),
+            num_frames=len(frames),
+            fps=5,
+        )
--- fish/edges.py
+++ fish/edges.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 import logging
 
 import dataclasses
@@ -7,13 +7,15 @@ import itertools
 import numpy as np
 import cv2 as cv
 
-from tqdm import tqdm
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+Frame = np.ndarray
 
-def get_edges(frame: np.ndarray, lower_threshold, upper_threshold, smoothing):
+
+def get_edges(
+    frame: Frame, lower_threshold: float, upper_threshold: float, smoothing: int
+):
     return cv.Canny(
         frame,
         threshold1=lower_threshold,
@@ -23,34 +25,38 @@ def get_edges(frame: np.ndarray, lower_threshold, upper_threshold, smoothing):
     )
 
 
-def vector_length(a, b) -> float:
+def vector_length(a: np.ndarray, b: np.ndarray) -> float:
     return np.linalg.norm(a - b)
 
 
-def get_contours(edges: np.ndarray):
+def get_contours(edges: np.ndarray, area_cutoff: int = 10):
     image, contours, hierarchy = cv.findContours(
         edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE
     )
 
-    filtered = []
-    for c in contours:
-        moments = cv.moments(c, binaryImage=True)
-        area = moments["m00"]
-        if area <= 10:
-            continue
+    contours = map(Contour, contours)
+    contours = [c for c in contours if c.area > area_cutoff]
 
-        filtered.append(c)
-
-    return filtered
+    return contours
 
 
 BLUE = (255, 0, 0)
 GREEN = (0, 255, 0)
 RED = (0, 0, 255)
+YELLOW = (0, 255, 255)
 
 
 class ObjectTracker:
-    def __init__(self):
+    def __init__(
+        self,
+        snap_to: int = 20,
+        lock_after: int = 10,
+        max_track_length: Optional[int] = None,
+    ):
+        self.snap_to = snap_to
+        self.lock_after = lock_after
+        self.max_track_length = max_track_length
+
         self.objects = {}
         self._id_counter = itertools.count()
         self.last_updated_frame = {}
@@ -58,12 +64,6 @@ class ObjectTracker:
 
     def _next_id(self):
         return next(self._id_counter)
-
-    def register(self, centroid, frame_idx):
-        id = self._next_id()
-        self.objects[id] = [centroid]
-        self.last_updated_frame[id] = frame_idx
-        self.locked[id] = False
 
     def update(self, centroids, frame_idx):
         if len(self.objects) == 0:
@@ -81,7 +81,7 @@ class ObjectTracker:
                 enumerate(centroids), key=lambda c: vector_length(c[1], last_position)
             )
 
-            if vector_length(closest_centroid, last_position) <= 20:
+            if vector_length(closest_centroid, last_position) <= self.snap_to:
                 positions.append(closest_centroid)
                 centroids.pop(centroid_idx)
                 self.last_updated_frame[object_id] = frame_idx
@@ -89,95 +89,131 @@ class ObjectTracker:
         for centroid in centroids:
             self.register(centroid, frame_idx)
 
+    def register(self, centroid, frame_idx):
+        id = self._next_id()
+        self.objects[id] = [centroid]
+        self.last_updated_frame[id] = frame_idx
+        self.locked[id] = False
+
     def clean(self, frame_idx):
-        self.objects = {oid: c[-100:] for oid, c in self.objects.items()}
+        if self.max_track_length is not None:
+            self.objects = {
+                oid: track[-self.max_track_length :]
+                for oid, track in self.objects.items()
+            }
         for oid in self.objects:
-            if self.last_updated_frame[oid] + 10 < frame_idx:
+            if self.last_updated_frame[oid] + self.lock_after < frame_idx:
                 self.locked[oid] = True
 
+    def track(self, contours, frame_idx):
+        centroids = [np.array([*c.xy]) for c in contours]
 
-def track_objects(object_tracker, contours, frame_idx):
-    centroids = []
-    for c in contours:
-        moments = cv.moments(c, binaryImage=True)
-        area = moments["m00"]
-        cx = int(moments["m10"] / area)
-        cy = int(moments["m01"] / area)
-        pos = np.array([cx, cy])
-        centroids.append(pos)
-
-    object_tracker.update(centroids, frame_idx)
+        self.update(centroids, frame_idx)
 
 
-def draw_contours(frame, contours):
-    centroids = []
-    areas = []
-    perimeters = []
-    boxes = []
-    for c in contours:
-        moments = cv.moments(c, binaryImage=True)
-        area = moments["m00"]
-        areas.append(area)
+@dataclasses.dataclass(frozen=True)
+class Contour:
+    contour: np.array
 
-        cx = int(moments["m10"] / area)
-        cy = int(moments["m01"] / area)
-        pos = np.array([cx, cy])
-        centroids.append(pos)
+    @property
+    def moments(self):
+        return cv.moments(self.contour, binaryImage=True)
 
-        perimeter = cv.arcLength(c, closed=True)
-        perimeters.append(round(perimeter, 2))
+    @property
+    def area(self):
+        return self.moments["m00"]
 
-        rect = cv.minAreaRect(c)
-        box = cv.boxPoints(rect)
-        box = np.int0(box)
-        boxes.append(box)
+    @property
+    def xy(self):
+        a = self.area
+        return self.moments["m10"] / a, self.moments["m01"] / a
 
+    @property
+    def xy_ints(self):
+        x, y = self.xy
+        return int(x), int(y)
+
+    @property
+    def perimeter(self):
+        return cv.arcLength(self.contour, closed=True)
+
+    @property
+    def bounding_rectangle(self):
+        return cv.minAreaRect(self.contour)
+
+
+def draw_bounding_rectangles(
+    frame: Frame,
+    contours: List[Contour],
+    mark_centroid: bool = False,
+    display_area: bool = False,
+    display_perimeter: bool = False,
+):
+    boxes = [cv.boxPoints(c.bounding_rectangle).astype(np.int0) for c in contours]
     cv.drawContours(frame, boxes, -1, color=GREEN, thickness=2, lineType=cv.LINE_AA)
-    for (x, y), area, perimeter in zip(centroids, areas, perimeters):
-        cv.rectangle(
-            frame,
-            (x - 1, y - 1),
-            (x + 1, y + 1),
-            color=BLUE,
-            thickness=-1,
-            lineType=cv.LINE_AA,
-        )
-        cv.putText(
-            frame,
-            str(area),
-            (x + 15, y - 15),
-            fontFace=cv.FONT_HERSHEY_DUPLEX,
-            fontScale=1,
-            color=BLUE,
-            thickness=1,
-            lineType=cv.LINE_AA,
-        )
-        cv.putText(
-            frame,
-            str(perimeter),
-            (x + 15, y + 15),
-            fontFace=cv.FONT_HERSHEY_DUPLEX,
-            fontScale=1,
-            color=GREEN,
-            thickness=1,
-            lineType=cv.LINE_AA,
-        )
+
+    for c in contours:
+        x, y = c.xy_ints
+        if mark_centroid:
+            cv.rectangle(
+                frame,
+                (x - 1, y - 1),
+                (x + 1, y + 1),
+                color=BLUE,
+                thickness=-1,
+                lineType=cv.LINE_AA,
+            )
+        if display_area:
+            cv.putText(
+                frame,
+                f"{int(c.area)}",
+                (x + 15, y - 15),
+                fontFace=cv.FONT_HERSHEY_DUPLEX,
+                fontScale=1,
+                color=BLUE,
+                thickness=1,
+                lineType=cv.LINE_AA,
+            )
+        if display_perimeter:
+            cv.putText(
+                frame,
+                f"{int(c.perimeter)}",
+                (x - 15, y - 15),
+                fontFace=cv.FONT_HERSHEY_DUPLEX,
+                fontScale=1,
+                color=GREEN,
+                thickness=1,
+                lineType=cv.LINE_AA,
+            )
 
     return frame
 
 
-def draw_objects(frame, object_tracker):
+def draw_live_object_tracks(frame: Frame, object_tracker: ObjectTracker):
+    object_id_to_tracks = {
+        oid: np.vstack(curve).astype(np.int0)
+        for oid, curve in object_tracker.objects.items()
+        if not object_tracker.locked[oid]
+    }
     cv.polylines(
         frame,
-        [
-            np.vstack(curve)
-            for oid, curve in object_tracker.objects.items()
-            if not object_tracker.locked[oid]
-        ],
+        list(object_id_to_tracks.values()),
         isClosed=False,
         color=RED,
-        thickness=1,
+        thickness=2,
         lineType=cv.LINE_AA,
     )
+
+    for oid, curve in object_id_to_tracks.items():
+        cv.putText(
+            frame,
+            f"{oid}",
+            (curve[-1, 0] + 15, curve[-1, 1] + 15),
+            fontFace=cv.FONT_HERSHEY_DUPLEX,
+            fontScale=1,
+            color=YELLOW,
+            thickness=1,
+            lineType=cv.LINE_AA,
+        )
 
     return frame
