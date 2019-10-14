e08b9de9afd3a226f13b9deb77b41771903fe6b5
JoshKarpel <karpel@wisc.edu>
2019-10-14 11:40:31 -0500

    more cleanup, more consistent domain model

--- dev/edges.py
+++ dev/edges.py
@@ -1,6 +1,7 @@
 import logging
 
 from pathlib import Path
+import itertools
 
 import numpy as np
 import cv2 as cv
@@ -17,16 +18,20 @@ def make_frames(frames, lower, upper, smoothing, draw_on_original=True):
     tracker = fish.ObjectTracker()
 
     for frame_idx, frame in enumerate(frames):
+        # produce the "modified" frame that we actually perform tracking on
         mod = backsub.apply(frame)
         mod = cv.morphologyEx(mod, cv.MORPH_OPEN, open_kernel)
         mod = cv.morphologyEx(mod, cv.MORPH_CLOSE, close_kernel)
+
+        # find edges, and from edges, contours
         edges = fish.get_edges(mod, lower, upper, smoothing)
         contours = fish.get_contours(edges)
 
         if frame_idx > 10:
-            tracker.track(contours, frame_idx)
+            tracker.update_tracks(contours, frame_idx)
             tracker.clean(frame_idx)
 
+        # produce the movie frame that we'll actually write out to disk
         img = cv.cvtColor((frame if draw_on_original else mod), cv.COLOR_GRAY2BGR)
         img = fish.draw_bounding_rectangles(img, contours)
         img = fish.draw_live_object_tracks(img, tracker)
@@ -38,12 +43,12 @@ if __name__ == "__main__":
     OUT = Path(__file__).parent / "out" / Path(__file__).stem
     OUT.mkdir(exist_ok=True)
 
-    for movie in ["drug", "control"]:
+    for movie, draw in itertools.product(["drug", "control"], (True, False)):
         frames = fish.load_or_read(IN / movie)[100:]
 
         op = fish.make_movie(
-            OUT / f"edge_test__{movie}",
-            frames=make_frames(frames, 100, 200, 5, draw_on_original=True),
+            OUT / f"edge_test__{movie}__draw_on_original={draw}",
+            frames=make_frames(frames, 100, 200, 5, draw_on_original=draw),
             num_frames=len(frames),
             fps=5,
         )
--- fish/edges.py
+++ fish/edges.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import Optional, List, Iterable, Tuple, Dict
 import logging
 
 import dataclasses
@@ -13,6 +13,36 @@ logger.setLevel(logging.DEBUG)
 Frame = np.ndarray
 
 
+@dataclasses.dataclass(frozen=True)
+class Contour:
+    contour: np.array
+
+    @property
+    def moments(self) -> Dict[str, float]:
+        return cv.moments(self.contour, binaryImage=True)
+
+    @property
+    def area(self) -> float:
+        return self.moments["m00"]
+
+    @property
+    def centroid(self) -> np.array:
+        a = self.area
+        return np.array([self.moments["m10"] / a, self.moments["m01"] / a])
+
+    @property
+    def centroid_ints(self) -> np.array:
+        return self.centroid.astype(np.int0)
+
+    @property
+    def perimeter(self) -> float:
+        return cv.arcLength(self.contour, closed=True)
+
+    @property
+    def bounding_rectangle(self):
+        return cv.minAreaRect(self.contour)
+
+
 def get_edges(
     frame: Frame, lower_threshold: float, upper_threshold: float, smoothing: int
 ):
@@ -25,11 +55,11 @@ def get_edges(
     )
 
 
-def vector_length(a: np.ndarray, b: np.ndarray) -> float:
+def distance_between(a: np.ndarray, b: np.ndarray) -> float:
     return np.linalg.norm(a - b)
 
 
-def get_contours(edges: np.ndarray, area_cutoff: int = 10):
+def get_contours(edges: np.ndarray, area_cutoff: float = 10):
     image, contours, hierarchy = cv.findContours(
         edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE
     )
@@ -46,6 +76,23 @@ RED = (0, 0, 255)
 YELLOW = (0, 255, 255)
 
 
+@dataclasses.dataclass
+class ObjectTrack:
+    frame_idxs: List[int] = dataclasses.field(default_factory=list)
+    positions: List[np.array] = dataclasses.field(default_factory=list)
+    areas: List[float] = dataclasses.field(default_factory=list)
+    is_locked: bool = False
+
+    def update(self, frame_idx: int, contour):
+        self.frame_idxs.append(frame_idx)
+        self.positions.append(contour.centroid)
+        self.areas.append(contour.area)
+
+    @property
+    def last_updated_frame(self) -> int:
+        return self.frame_idxs[-1]
+
+
 class ObjectTracker:
     def __init__(
         self,
@@ -57,103 +104,74 @@ class ObjectTracker:
         self.lock_after = lock_after
         self.max_track_length = max_track_length
 
-        self.objects = {}
+        self.tracks = {}
         self._id_counter = itertools.count()
-        self.last_updated_frame = {}
-        self.locked = {}
 
     def _next_id(self):
         return next(self._id_counter)
 
-    def update(self, centroids, frame_idx):
-        if len(self.objects) == 0:
-            for centroid in centroids:
-                self.register(centroid, frame_idx)
+    def update_tracks(self, contours, frame_idx):
+        # if we have no tracks yet, just make everything a track
+        if len(self.tracks) == 0:
+            for contour in contours:
+                self.register(frame_idx, contour)
+            return
 
-        centroids = centroids[:]
-        for object_id, positions in (
-            (oid, pos) for oid, pos in self.objects.items() if not self.locked[oid]
-        ):
-            if len(centroids) == 0:
+        # copy the list of centroids, since we're about to mutate it
+        contours = contours[:]
+
+        # try to assign a contour to each unlocked object track
+        for track in (track for track in self.tracks.values() if not track.is_locked):
+            if len(contours) == 0:
                 return
-            last_position = positions[-1]
-            centroid_idx, closest_centroid = min(
-                enumerate(centroids), key=lambda c: vector_length(c[1], last_position)
+            last_position = track.positions[-1]
+            closest_centroid_idx, closest_centroid = min(
+                enumerate(contours),
+                key=lambda c: distance_between(c[1].centroid, last_position),
             )
 
-            if vector_length(closest_centroid, last_position) <= self.snap_to:
-                positions.append(closest_centroid)
-                centroids.pop(centroid_idx)
-                self.last_updated_frame[object_id] = frame_idx
+            if (
+                distance_between(closest_centroid.centroid, last_position)
+                <= self.snap_to
+            ):
+                track.update(frame_idx, closest_centroid)
+                contours.pop(closest_centroid_idx)
 
-        for centroid in centroids:
-            self.register(centroid, frame_idx)
+        # register leftover centroids as new objects
+        for contour in contours:
+            self.register(frame_idx, contour)
 
-    def register(self, centroid, frame_idx):
+    def register(self, frame_idx: int, contour):
         id = self._next_id()
-        self.objects[id] = [centroid]
-        self.last_updated_frame[id] = frame_idx
-        self.locked[id] = False
+        track = ObjectTrack()
+        track.update(frame_idx, contour)
+        self.tracks[id] = track
 
-    def clean(self, frame_idx):
+    def clean(self, frame_idx: int):
         if self.max_track_length is not None:
-            self.objects = {
+            self.tracks = {
                 oid: track[-self.max_track_length :]
-                for oid, track in self.objects.items()
+                for oid, track in self.tracks.items()
             }
-        for oid in self.objects:
-            if self.last_updated_frame[oid] + self.lock_after < frame_idx:
-                self.locked[oid] = True
-
-    def track(self, contours, frame_idx):
-        centroids = [np.array([*c.xy]) for c in contours]
-
-        self.update(centroids, frame_idx)
-
-
-@dataclasses.dataclass(frozen=True)
-class Contour:
-    contour: np.array
-
-    @property
-    def moments(self):
-        return cv.moments(self.contour, binaryImage=True)
-
-    @property
-    def area(self):
-        return self.moments["m00"]
-
-    @property
-    def xy(self):
-        a = self.area
-        return self.moments["m10"] / a, self.moments["m01"] / a
-
-    @property
-    def xy_ints(self):
-        x, y = self.xy
-        return int(x), int(y)
-
-    @property
-    def perimeter(self):
-        return cv.arcLength(self.contour, closed=True)
-
-    @property
-    def bounding_rectangle(self):
-        return cv.minAreaRect(self.contour)
+        for oid, track in self.tracks.items():
+            if track.last_updated_frame + self.lock_after < frame_idx:
+                track.is_locked = True
 
 
 def draw_bounding_rectangles(
     frame: Frame,
-    contours: List[Contour],
+    contours: Iterable[Contour],
     mark_centroid: bool = False,
     display_area: bool = False,
     display_perimeter: bool = False,
 ):
+    contours = list(contours)
+
     boxes = [cv.boxPoints(c.bounding_rectangle).astype(np.int0) for c in contours]
     cv.drawContours(frame, boxes, -1, color=GREEN, thickness=2, lineType=cv.LINE_AA)
 
     for c in contours:
-        x, y = c.xy_ints
+        x, y = c.centroid_ints
         if mark_centroid:
             cv.rectangle(
                 frame,
@@ -190,21 +208,21 @@ def draw_bounding_rectangles(
 
 
 def draw_live_object_tracks(frame: Frame, object_tracker: ObjectTracker):
-    object_id_to_tracks = {
-        oid: np.vstack(curve).astype(np.int0)
-        for oid, curve in object_tracker.objects.items()
-        if not object_tracker.locked[oid]
+    object_id_to_track = {
+        oid: np.vstack(track.positions).astype(np.int0)
+        for oid, track in object_tracker.tracks.items()
+        if not track.is_locked
     }
     cv.polylines(
         frame,
-        list(object_id_to_tracks.values()),
+        list(object_id_to_track.values()),
         isClosed=False,
         color=RED,
         thickness=2,
         lineType=cv.LINE_AA,
     )
 
-    for oid, curve in object_id_to_tracks.items():
+    for oid, curve in object_id_to_track.items():
         cv.putText(
             frame,
             f"{oid}",
